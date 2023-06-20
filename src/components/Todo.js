2d7796edd15aa23f3cfbf51d16e2ee106a140233
Alberto <66646508+PeralStudio@users.noreply.github.com>
2023-06-20 08:04:24 +0200

    Cursor pointer only where needed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,28 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
-export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
- 
-  return (
-    <div className="Todo">
-        <p className={`${task.completed ? 'completed' : ""}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
-        <div>
-        <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)} />
-        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+export const Todo = ({ task, deleteTodo, editTodo, toggleComplete }) => {
+    return (
+        <div className="Todo">
+            <p
+                className={`${task.completed ? "completed" : "incompleted"}`}
+                onClick={() => toggleComplete(task.id)}
+            >
+                {task.task}
+            </p>
+            <div>
+                <FontAwesomeIcon
+                    className="edit-icon"
+                    icon={faPenToSquare}
+                    onClick={() => editTodo(task.id)}
+                />
+                <FontAwesomeIcon
+                    className="delete-icon"
+                    icon={faTrash}
+                    onClick={() => deleteTodo(task.id)}
+                />
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
